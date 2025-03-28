a297e0240663a4b7383a09fbf8fcf720641d4346
Jurik Vit <jurik@gymvod.local>
2025-03-28 11:21:38 +0100

    pokracovani v ukolu

--- UkolForms/UkolForms/Form1.Designer.cs
+++ UkolForms/UkolForms/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(126, 444);
+            this.button1.Location = new System.Drawing.Point(872, 379);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(65, 47);
             this.button1.TabIndex = 0;
@@ -41,11 +43,31 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(925, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(85, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(695, 551);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1384, 874);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +78,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- UkolForms/UkolForms/Form1.cs
+++ UkolForms/UkolForms/Form1.cs
@@ -15,19 +15,32 @@ namespace UkolForms
 
         public Point currentPoint = new Point();
         public Point oldPoint = new Point();    
-        public Pen pen = new Pen(Color.DarkRed, 1);
-        public Graphics g;
+        public Pen pen1 = new Pen(Color.DarkRed, 2);
+        public Graphics panelGraphics;
         public Form1()
         {
             InitializeComponent();
-            g = CreateGraphics();
 
+            panelGraphics = panel1.CreateGraphics();
+            pen1.SetLineCap(System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.LineCap.Round, System.Drawing.Drawing2D.DashCap.Round);
         }
 
 
         private void button1_Click(object sender, EventArgs e)
         {
+            panelGraphics.DrawEllipse(pen1, 100, 100, 400, 100);
+            button1.Size = new Size(button1.Width + 100, button1.Height + 100);
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panelGraphics.DrawArc(pen1, 100, 100, 300, 300, 40, 40);
+            panelGraphics.Clear(Color.White);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
         }
     }
 }
