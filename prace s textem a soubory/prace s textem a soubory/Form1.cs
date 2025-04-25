f94636c2cea3c0c3008e37c844f1698dfcf5c975
Jurik Vit <jurik@gymvod.local>
2025-04-25 11:18:06 +0200

    prace s textem

---
+++ prace s textem a soubory/prace s textem a soubory/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prace_s_textem_a_soubory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toUpper_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToUpper();
+        }
+
+        private void toLower_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToLower();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string[] words = textBox1.Text.Split(' ');
+            textBox1.Clear();
+            foreach (string word in words)
+            {
+                textBox1.Text += word;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            int position = int.Parse(textBox2.Text) + 1;
+            string text = textBox1.Text;
+            string newText = text.Insert(position, textBox3.Text);
+            textBox1.Text  = newText;
+
+        }
+    }
+}
